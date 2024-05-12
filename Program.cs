73a0ebb35a11825b5893ca78460a4aa41a55c32f
Diamantine <meteoritus75@gmail.com>
2024-05-12 12:50:52 +0200

    Challenge dictionnaire

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Challenge_Dictionnaire
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> mots = new Dictionary<string, string>()
+            {
+                { "a", "bonjour" },
+                { "b", "hello" },
+                { "c", "salut" }
+
+            };
+            foreach (var mot in mots)
+            {
+                Console.WriteLine(mot.Value);
+            }
+        }
+    }
+}
